03139194113bb2d39de7504e27c97cf4261a7104
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-22 19:39:01 +0000

    feat: import and configure dotenv

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
 import fastify from "fastify";
 
+dotenv.config();
 const fastifyApp =  fastify();
 
-fastifyApp.listen({ port: 8080 }).then(() => console.log(`HTTP Server running on PORT`));
+fastifyApp.listen({ port: process.env.PORT }).then(() => console.log(`HTTP Server running on PORT ${process.env.PORT}`));
