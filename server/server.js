e0a99efe40e9f514bb6cd03608fdb8b8d5090349
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-22 19:59:14 +0000

    feat: register and define a prefix for the API routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import fastify from "fastify";
+import routes from "./src/routes/apiRoutes.js";
 
 dotenv.config();
 const fastifyApp =  fastify();
+
+fastifyApp.register(routes, { prefix: '/sportz' });
 
 fastifyApp.listen({ port: process.env.PORT }).then(() => console.log(`HTTP Server running on PORT ${process.env.PORT}`));
