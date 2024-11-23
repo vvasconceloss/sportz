b40d3e9caa5a206dc8d786d12b0da8588a95a2e2
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-23 10:41:30 +0000

    feat: create the aside component for the popular leagues (template)

---
+++ client/src/components/asidePopularLeagues/asidePopularLeagues.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import SportzImageLogo from '../../assets/sportz.png';
+import { 
+  AsideContainer, 
+  AsideLeagues, 
+  AsideTitle, 
+  League, 
+  LeagueImage, 
+  LeagueTitle, 
+  LinkStyled } from "./asidePopuarLeague.styles";
+
+export const AsidePopularLeagues = () => {
+  let leagues = ['Champions League', 'LaLiga', 'Bundesliga', 'Premier League', 'Premier League', 'Premier League',  'Premier League'];
+
+  return (
+    <AsideContainer>
+      <AsideTitle>Popular Leagues</AsideTitle>
+      <AsideLeagues>
+        {
+          leagues.map((league, key) => (
+            <Link className={LinkStyled()} to={'/'}>
+              <League key={key}>
+                <LeagueImage src={SportzImageLogo} />
+                <LeagueTitle>{league}</LeagueTitle>
+              </League>
+            </Link>
+          ))
+        }
+      </AsideLeagues>
+    </AsideContainer>
+  );
+}
