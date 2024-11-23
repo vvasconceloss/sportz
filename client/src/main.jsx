4a5741e4c99b98c8ca26bbf4caa600c6e879e9da
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-23 10:59:19 +0000

    fix: remove unnecessary dotenv configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import dotnev from "dotenv";
 import { createRoot } from 'react-dom/client';
 import { globalStyles } from './styles/global';
 import { AppRoutes } from './routes/routes.jsx';
 
 globalStyles();
-dotnev.config();
 
 createRoot(document.getElementById('root')).render( <AppRoutes /> );
