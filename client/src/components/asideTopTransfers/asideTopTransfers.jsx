7d4d212f6306bc46b3ef2bfca6fa02653954e3a4
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-24 09:39:21 +0000

    feat: consume data from top transfers in the aside

--- client/src/components/asideTopTransfers/asideTopTransfers.jsx
+++ client/src/components/asideTopTransfers/asideTopTransfers.jsx
@@ -1,4 +1,5 @@
-import SportzImageLogo from '../../assets/sportz.png';
+import { useEffect, useState } from 'react';
+import { getTopMarketValueTransfer } from '../../services/playersApi';
 import { 
   AsideContainer, 
   AsideTitle, 
@@ -9,28 +10,52 @@ import {
   PlayerTransferInfo, 
   Transfer, 
   TransferInfo, 
-  TransferTop } from "./asideTopTransfers.style"
+  TransferTop,
+  TransferHeader, 
+  ClubImages} from "./asideTopTransfers.style"
 
 export const AsideTopTransfers = () => {
+  const [topTransfers, setTopTransfers] = useState([]);
+
+  const defineTopTransfers = async () => {
+    const responseData = await getTopMarketValueTransfer();
+    setTopTransfers(responseData?.transfers);
+  }
+
+  useEffect(() => {
+    defineTopTransfers();
+  }, []);
+
   return (
     <AsideContainer>
       <AsideTitle>Top Transfers</AsideTitle>
       <AsideTransfers>
-        <Transfer>
-          <TransferTop>
-            <PlayerImage src={SportzImageLogo} />
-            <TransferInfo>
-              <PlayerDetails>Lionel Messi</PlayerDetails>
+        {
+          topTransfers?.map((transfer) => (
+          <Transfer key={transfer.transfer.playerId}>
+            <TransferHeader>
+              <PlayerDetails>{transfer.transfer.name}</PlayerDetails>
+              <PlayerDetails>{transfer.detail.playerDetails[2].value.numberValue} years old</PlayerDetails>
+            </TransferHeader>
+            <TransferTop>
+              <PlayerImage src={transfer.image.playerImage} />
+              <TransferInfo>
+                <PlayerDetails>{transfer.transfer.position.label} | {transfer.detail.playerDetails[4].value.fallback}</PlayerDetails>
               </TransferInfo>
-          </TransferTop>
-          <PlayerTransferInfo>
-              <PlayerDetails>€50.00M</PlayerDetails>
-              <div className="club-images">
-                <PlayerClubLogo src={SportzImageLogo} />
-                <PlayerClubLogo src={SportzImageLogo} />
-              </div>
+            </TransferTop>
+            <PlayerTransferInfo>
+                <PlayerDetails>{
+                  transfer.transfer.fee.value 
+                  ? new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'EUR',}).format(transfer.transfer.fee.value) 
+                  : transfer.transfer.fee.feeText.toUpperCase()}</PlayerDetails>
+                  <ClubImages>
+                    <PlayerClubLogo src={transfer.fromClub} />
+                    <PlayerClubLogo src={transfer.toClub} />
+                  </ClubImages>
             </PlayerTransferInfo>
-        </Transfer>
+          </Transfer>
+          ))
+        }
       </AsideTransfers>
     </AsideContainer>
   )
